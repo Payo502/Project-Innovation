ced4d2e8a4c42490a7570077fdaaa82e61b56ac8
tediveleva <tediveleva@gmail.com>
2024-02-28 17:32:39 +0100

    yes

---
+++ Assets/GameManager.cs
@@ -0,0 +1,58 @@
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class GameManager : MonoBehaviour
+{
+    [SerializeField] private GameObject gameOverUI;
+
+    public bool hasGameEnded = false;
+
+    public float restartDelay = 1f;
+
+    private void Start()
+    {
+        Cursor.visible = false;
+        Cursor.lockState = CursorLockMode.Locked;
+    }
+
+    private void Update()
+    {
+        if (gameOverUI.activeInHierarchy)
+        {
+            Cursor.visible = true;
+            Cursor.lockState = CursorLockMode.None;
+        }
+        else
+        {
+            Cursor.visible = false;
+            Cursor.lockState = CursorLockMode.Locked;
+        }
+    }
+
+    public void EndGame()
+    {
+        if (!hasGameEnded)
+        {
+            gameOverUI.SetActive(true);
+
+            hasGameEnded = true;
+        }
+    }
+
+    public void RestartGame()
+    {
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex);
+    }
+
+    public void QuitGame()
+    {
+        Application.Quit();
+    }
+
+    public void MainMenu()
+    {
+        // WHEN MAIN MENU SCENE IS MADE UNCOMMENT
+        //SceneManager.LoadScene("MainMenu");
+        Debug.Log("MAIN MENU");
+    }
+}
---
+++ Assets/scripts/Enums.cs
@@ -0,0 +1,18 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public enum playerstate
+{
+    move,
+    stop,
+    phone,
+    machine
+};
+
+public enum Frequency
+{
+    frequency01,
+    frequency02,
+    frequency03,
+}
