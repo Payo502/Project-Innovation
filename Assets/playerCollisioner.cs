f17c969804d3a535cf325ca2428bf09a20f757b0
Payo502 <patriciogutierrez236@gmail.com>
2024-03-01 14:53:29 +0100

    Fixed Everything to build

--- Assets/playerCollisioner.cs
+++ Assets/playerCollisioner.cs
@@ -4,18 +4,6 @@ using UnityEngine;
 
 public class playerCollisioner : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
-
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
-
     private void OnTriggerEnter(Collider other)
     {
         var comp = other.gameObject.GetComponent<audiotrigger>();
