fe114990ee043f680f32bdea4cca71002d736063
Payo502 <patriciogutierrez236@gmail.com>
2024-03-04 13:48:44 +0100

    Implemented the speaker functionality

--- Assets/scripts/PlayerState.cs
+++ Assets/scripts/PlayerState.cs
@@ -72,13 +72,8 @@ public class PlayerState : MonoBehaviour
 
         if (Input.GetKeyDown("space"))
         {
-            GameObject.Find("networkManager").GetComponent<ServerMessageManager>().SendStringMessagesToClient(ServerToClientId.stringMessage, "tape1");
+            GameObject.Find("NetworkManager").GetComponent<ServerMessageManager>().SendStringMessagesToClient(ServerToClientId.stringMessage, "tape1");
 
-        }
-
-        if (Input.GetKeyDown(KeyCode.G))
-        {
-            DirectGuardsToLocation(guardsLocation.position);
         }
     }
 
@@ -114,5 +109,14 @@ public class PlayerState : MonoBehaviour
     public void ChangeFrequency(Frequency newFrequency)
     {
         currentFrequency = newFrequency;
+    }
+
+    public void ScreamReceived(bool screamReceived)
+    {
+        if (screamReceived)
+        {
+            Debug.Log("Directing Guards to location");
+            DirectGuardsToLocation(guardsLocation.position);
+        }
     }
 }
